3a53eca167069e954d6ba62d5038bc8e8a9ee623
Oleg196-Ek <zhookov@mail.ru>
2024-09-19 18:40:32 +0300

    Add files via upload

---
+++ module 2.2.py
@@ -0,0 +1,9 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and third == second:
+    print(3)
+if first == second or first == third:
+    print(2)
+else:
+    print(0)
